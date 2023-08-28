8dafae568e7b97724626383641a4163d2664e261
Paultamna <paultamba1997@gmail.com>
2023-08-28 14:38:10 +0300

    Am adding files as a Senior of the project

---
+++ startUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Evaralda = () => {
+    return(
+        <>
+        <Text>If you have seen this, then you might be either</Text>
+        <Text>Evaralda- Miss Coict</Text>
+        <Text>Omary Homera-Mbeya Boy</Text>
+        <Text>Ernest from Lindi</Text>
+        </>
+    )
+}
