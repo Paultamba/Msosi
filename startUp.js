fd389cb7cea8c423b697f5a4b55ee29d22f4cdca
PeachAlda <102580416+PeachAlda@users.noreply.github.com>
2023-09-04 07:02:56 +0300

    Update startUp.js

--- startUp.js
+++ startUp.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-export const Evaralda = () => {
+export const Everalda = () => {
     return(
         <>
         <Text>If you have seen this, then you might be either</Text>
         <Text>Evaralda- Miss Coict</Text>
         <Text>Omary Homera-Mbeya Boy</Text>
         <Text>Ernest from Lindi</Text>
-        <Text>Please make sure you chenge git branch before you push anything in this repository</Text>
+        <Text>Please make sure you change git branch before you push anything in this repository</Text>
         </>
     )
 }
